3f84b49a6f5f9119110257e1bb1049f787c9f2a6
Joel Tio <joeltiojinhon@gmail.com>
2021-09-23 08:38:22 +0800

    feat: add button to change color

--- simple-app/app/src/main/java/com/sst/testapplication/MainActivity.kt
+++ simple-app/app/src/main/java/com/sst/testapplication/MainActivity.kt
@@ -2,10 +2,29 @@ package com.sst.testapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    val colors = listOf(R.color.sst_red, R.color.sst_blue, R.color.sst_grey)
+    var currentColor = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val parentLayout = findViewById<ConstraintLayout>(R.id.parent_constraintLayout)
+        val changeColorButton = findViewById<Button>(R.id.change_color_button)
+
+        // Set the current color
+        parentLayout.setBackgroundColor(ContextCompat.getColor(this, colors[currentColor]))
+
+        // When the button is clicked, loop through the colors
+        changeColorButton.setOnClickListener {
+            currentColor = (currentColor + 1) % colors.size
+            parentLayout.setBackgroundColor(ContextCompat.getColor(this, colors[currentColor]))
+        }
     }
 }
